a506cc926509d96036729df2e393ef9f4ebcc585
Shinx99 <nguyennam.zarakhan@gmail.com>
2025-09-23 22:51:53 +0700

    add version Lombok and update maven

--- src/main/java/com/asm/ecommerce/product/Controller/ProductController.java
+++ src/main/java/com/asm/ecommerce/product/Controller/ProductController.java
@@ -9,7 +9,7 @@ public class ProductController {
 
     @RequestMapping("/hello/sayhi")
     public String sayHi(Model model){
-        model.addAttribute("sayhy","hi hi hi heehehehihihihaahahahhaha hahahhahhahahah!");
+        model.addAttribute("sayhy","hi hi hi heehehehihihihaahahahhaha hahahhahhahahah hihihihi!");
         model.addAttribute("subject","Spring Boot MVC - test connection ");
 
         return "hello";
